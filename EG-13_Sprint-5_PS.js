e8b15e3b99b59cb7238d3d58c94976d015878212
saikot05 <saikotislam08@gmail.com>
2026-09-19 23:26:43 +0600

    Solved binary search and search insert position

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -15,3 +15,52 @@ var removeDuplicates = function(nums) {
     }
     return writeIndex;
 };
+//02. Binary Search
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        // Equivalent to Math.floor((left + right) / 2), avoiding integer overflow
+        const mid = left + Math.floor((right - left) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
+//03. Search Insert Position
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = left + Math.floor((right - left) / 2);
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
