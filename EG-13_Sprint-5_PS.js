2af3713116544c4259f0fb0bd5301e03b94868ca
saikot05 <saikotislam08@gmail.com>
2026-09-19 23:03:01 +0600

    Solve remove duplicate

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -4,6 +4,14 @@
  * @param {number} k
  * @return {boolean}
  */
-var checkSubarraySum = function(nums, k) {
-
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    let writeIndex = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[writeIndex - 1]) {
+            nums[writeIndex] = nums[i];
+            writeIndex++;
+        }
+    }
+    return writeIndex;
 };
